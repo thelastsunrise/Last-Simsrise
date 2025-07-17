17050b7533c76036fa995dc7e3c0d303b6701ea7
Cat <cat@lastsunrise.art>
2025-07-17 19:33:47 -0400

    Install astro.db

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,9 @@ import { defineConfig } from 'astro/config';
 
 import mdx from '@astrojs/mdx';
 
+import db from '@astrojs/db';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()]
+  integrations: [mdx(), db()]
 });
